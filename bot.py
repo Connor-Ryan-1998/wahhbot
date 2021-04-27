652488a5f4c3f3053e72e0757feb29be47d9068a
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-27 14:49:05 +1000

    Adding initial files

---
+++ bot.py
@@ -0,0 +1,95 @@
+# bot.py
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+from random import choice
+from utils import *
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+bot = commands.Bot(command_prefix='$')
+
+kicking_users = []
+# class MyClient(discord.Client):
+#     async def on_message(self, message):
+#         if message.author == self.user:
+#             return
+
+#         if message.content.startswith('$hello'):
+#             await message.channel.send('Hello World!')
+
+channel = 'dota-content'
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # if (message.channel == 'mouse-verification' and isinstance(message.content, str)):
+    #     print('sukk')
+    #     for guild in client.guilds:
+    #         for member in guild.members:
+    #             if (member.name == message.author.name):
+    #                 await member.move_to(None)   
+    #                 await message.channel.send('fuck you dick boy thats not a mouse')
+
+    isVoteInitiated = False
+    
+    if (isinstance(message.content, str)):
+        await message.channel.send("'" + ''.join(choice((str.upper, str.lower))(c) for c in message.content) + "'" +  " - " +  message.author.name)
+        # if (message.author.name == 'lachyme'):
+        #     await message.channel.send('nah fuck u gay yellow man')
+        #     for guild in client.guilds:
+        #         for member in guild.members:
+        #             if (member.name == message.author.name):
+        #                 await member.move_to(None)  
+        # else:
+        #     isVoteInitiated = True
+        #     await message.channel.send('Vote initiated 1/2')
+
+    
+    # if message.content == 'please' and isVoteInitiated:
+    #     await message.channel.send('whoops, suck to suck mini dicklin 2/2')
+    #     for guild in client.guilds:
+    #         for member in guild.members:
+    #             if (member.name == 'Reanu Keeves'):
+    #                 await member.move_to(None)    
+
+@bot.command()
+async def kick(ctx, target_user:discord.User):
+
+    # await require_lower_permissions(ctx, target_user, bot)
+
+    if target_user in kicking_users:
+        await ctx.send("There is already a kick vote on `{}`!".format(target_user))
+        return 
+
+    # add to kicking_users
+    kicking_users.append(target_user)
+
+    vote_passed = await take_vote(ctx, "Kick `{}`?\n⚠ NOTE: Can't kick users with an equal or higher role.".format(target_user), KICK_VOTE_TIME, MIN_KICK_VOTERS)
+
+    if vote_passed:
+        try:
+            await ctx.guild.kick(target_user)
+            await ctx.send("👢 Kicked `{}`.".format(target_user))
+        except discord.ext.commands.errors.CommandInvokeError:
+            await error_admin_targeted(ctx)
+
+    kicking_users.remove(target_user)
+client.run(TOKEN)
+
+
---
+++ utils.py
@@ -0,0 +1,56 @@
+import random
+import discord
+from discord.ext import commands
+import asyncio
+
+async def take_vote(ctx, question:str, vote_time, min_voters):
+
+    """
+    take_vote(ctx, question:str) - Collects votes
+    ctx: pass from command function
+    question: what to ask
+    returns [<all who want>, <all who don't want>]. 
+    It's up to the context/use case to decide how these should be used.
+    """
+
+    embed_title="NEW VOTE"
+    votey_message = await ctx.send(
+        embed=discord.Embed(
+            type="rich",
+            title=embed_title,
+            # description="{}\n\n✅ - Yes\n\n❌ - No".format(question)
+            description="{0}\n\n✅ - Yes\n❌ - No\nMinimum {1} votes required.".format(question, min_voters)
+        )
+    )
+
+    await votey_message.add_reaction('✅')
+    await votey_message.add_reaction('❌')
+
+    passed = False
+    curr_time = 0
+    while curr_time < vote_time:
+        await asyncio.sleep(5)
+        all_in_favor = 0
+        not_in_favor = 0
+        finished_votey = await votey_message.channel.fetch_message(votey_message.id)
+        for reaction in finished_votey.reactions:
+            if str(reaction.emoji) == '✅':
+                all_in_favor += reaction.count-1 # don't include bot's reaction
+            elif str(reaction.emoji) == '❌':
+                not_in_favor += reaction.count-1
+
+        if all_in_favor > not_in_favor and all_in_favor >= min_voters:
+            passed = True
+            break
+
+        await asyncio.sleep(5)
+        curr_time += 5
+
+    question += "\nVERDICT: **Vote passed!**" if passed else "\nVERDICT: **Vote failed!**"
+
+    await votey_message.edit(embed=discord.Embed(
+        type="rich",
+        title=embed_title,
+        description=question
+    ))
+    return passed
