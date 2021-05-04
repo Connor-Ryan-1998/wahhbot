cae85f7178675e98087d0c0bdd0984f36e354fb9
Connor Ryan <Work1998@hotmail.co.uk>
2021-05-04 21:48:43 +1000

    Cleaned up and deployed

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python Utility Bot
+
+Wahh Bot, built to abuse
+
+[Join the server](https://discord.gg/7HbmCX9vEu) if you're interested in helping out.
--- bot.py
+++ bot.py
@@ -29,59 +29,10 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    # if (message.channel == 'mouse-verification' and isinstance(message.content, str)):
-    #     print('sukk')
-    #     for guild in client.guilds:
-    #         for member in guild.members:
-    #             if (member.name == message.author.name):
-    #                 await member.move_to(None)   
-    #                 await message.channel.send('fuck you dick boy thats not a mouse')
-
     isVoteInitiated = False
     
     if (isinstance(message.content, str)):
         await message.channel.send("'" + ''.join(choice((str.upper, str.lower))(c) for c in message.content) + "'" +  " - " +  message.author.name)
-        # if (message.author.name == 'lachyme'):
-        #     await message.channel.send('nah fuck u gay yellow man')
-        #     for guild in client.guilds:
-        #         for member in guild.members:
-        #             if (member.name == message.author.name):
-        #                 await member.move_to(None)  
-        # else:
-        #     isVoteInitiated = True
-        #     await message.channel.send('Vote initiated 1/2')
-
-    
-    # if message.content == 'please' and isVoteInitiated:
-    #     await message.channel.send('whoops, suck to suck mini dicklin 2/2')
-    #     for guild in client.guilds:
-    #         for member in guild.members:
-    #             if (member.name == 'Reanu Keeves'):
-    #                 await member.move_to(None)    
-
-# @bot.command()
-# async def kick(ctx, target_user:discord.User):
-
-#     # await require_lower_permissions(ctx, target_user, bot)
-
-#     if target_user in kicking_users:
-#         await ctx.send("There is already a kick vote on `{}`!".format(target_user))
-#         return 
-
-#     # add to kicking_users
-#     kicking_users.append(target_user)
-
-#     vote_passed = await take_vote(ctx, "Kick `{}`?\n⚠ NOTE: Can't kick users with an equal or higher role.".format(target_user), KICK_VOTE_TIME, MIN_KICK_VOTERS)
-
-#     if vote_passed:
-#         try:
-#             await ctx.guild.kick(target_user)
-#             await ctx.send("👢 Kicked `{}`.".format(target_user))
-#         except discord.ext.commands.errors.CommandInvokeError:
-#             await error_admin_targeted(ctx)
-
-#     kicking_users.remove(target_user)
 client.run(TOKEN)
 
 
